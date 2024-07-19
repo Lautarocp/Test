b64472627893cf8b3c1018e0cd9f3ea3f5c9906b
Lautarocp <lautarocp20@gmail.com>
2024-07-19 16:02:33 +0200

    nueva carpeta practica

--- Ejemplos con ajax/Ejemplo-ajax.js
+++ Ejemplos con ajax/Ejemplo-ajax.js
@@ -5,21 +5,19 @@ function reqListener() {
     const usuarioRender = usuarios
         .map(usuario=>`<li>${usuario.nombre}</li>`)
         .join("");
-        console.log(usuarioRender);
         lista.innerHTML= usuarioRender;
 };
 
 
 var oReq = new XMLHttpRequest();
 oReq.addEventListener("load", reqListener);
-oReq.open("GET","http://127.0.0.1:5500/Ejemplos%20con%20ajax/usuarios.json");
+oReq.open("GET","https://raw.githubusercontent.com/Lautarocp/Testing/main/Ejemplos%20con%20ajax/usuarios.json");
 oReq.send();
 
-function enviarDatos(){
-    oReq.open("POST", "http://127.0.0.1:5500/Ejemplos%20con%20ajax/usuarios.json",true);
-    oReq.setRequestHeader("Content-Type",
-        "aplication/x-www-form-urlencoded"
+function enviarDatos()
+{     oReq.open("POST", "https://raw.githubusercontent.com/Lautarocp/Testing/main/Ejemplos%20con%20ajax/usuarios.json",true);
+    oReq.setRequestHeader("Content-Type",         "aplication/x-www-form-urlencoded"
     );
-    oReq.send("nombre=sabado22")
+     oReq.send("nombre=sabado22")
 }
-boton.onclick = enviarDatos;
+ boton.onclick = enviarDatos
